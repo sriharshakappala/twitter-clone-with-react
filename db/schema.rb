9e7c558b89fd9a26e8dd9233b2f9d730e15babaa
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-10-21 07:59:31 +0530

    Follow API call

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017014829) do
+ActiveRecord::Schema.define(version: 20161020155139) do
+
+  create_table "followers", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "followed_by"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer  "user_id"
